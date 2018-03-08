dc206d1ee2e61b7ca71924759dc59172eca2d28c
ivmartel <ivmartel@gmail.com>
2018-03-08 23:33:49 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+/* global module */
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'service-worker.js', 'src/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Task to run tests
+    grunt.registerTask('publish', ['jshint']);
+};
---
+++ readme.md
@@ -0,0 +1,10 @@
+dwv-jqmobile
+============
+
+Medical viewer using [DWV](https://github.com/ivmartel/dwv) (DICOM Web Viewer) and [jQuery mobile](https://jquerymobile.com/).
+
+All coding/implementation contributions and comments are welcome.
+
+DWV-jqmobile is not certified for diagnostic use. Released under GNU GPL v3 license (see [license.txt](license.txt)).
+
+[![Build Status](https://travis-ci.org/ivmartel/dwv-jqmobile.svg?branch=master)](https://travis-ci.org/ivmartel/dwv-jqmobile) 
