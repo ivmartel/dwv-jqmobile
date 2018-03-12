0ce1d9bfde73dfadaff57c7ba91e5b0b203d8ecc
ivmartel <ivmartel@gmail.com>
2018-03-12 20:32:05 +0100

    Fix update script. Add gitattributes.

--- resources/scripts/update-gh-pages.sh
+++ resources/scripts/update-gh-pages.sh
@@ -2,26 +2,29 @@
 #Script to push build results on the repository gh-pages branch.
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  #we should be in /home/travis/build/ivmartel/dwv-jqmobile
+  # we should be in /home/travis/build/ivmartel/dwv-jqmobile
   echo -e "Starting to update gh-pages\n"
-  #clean up node_modules
-  rm -Rf node_modules
-  yarn install --prod
-  #go to home and setup git
+  # go to home and setup git
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv-jqmobile.git gh-pages > /dev/null
-  #go into directory and copy data we're interested in to that directory
-  cd gh-pages
-  #copy new build
-  cp -Rf $HOME/build/ivmartel/dwv-jqmobile/* demo/trunk
+  # using token, clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv-jqmobile.git gh-pages
+  # change dir to demo
+  cd $HOME/gh-pages/demo/trunk
+  # copy new build here
+  cp -Rf $HOME/build/ivmartel/dwv-jqmobile/* .
   # remove gitignore
-  rm -f demo/trunk/.gitignore
-  #add, commit and push files
+  rm -f .gitignore
+  # clean up node_modules
+  rm -Rf node_modules/*
+  # download production dependencies
+  yarn install --prod
+  # move back to root of repo
+  cd $HOME/gh-pages
+  # add, commit and push files
   git add -Af .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages
   echo -e "Done updating.\n"
 fi
