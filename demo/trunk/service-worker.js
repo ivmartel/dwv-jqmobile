b065946ca3da610cd58df36efd7d5b84ca9eb092
Travis <travis@travis-ci.org>
2018-04-05 21:45:04 +0000

    Travis build 46 pushed to gh-pages

--- demo/trunk/service-worker.js
+++ demo/trunk/service-worker.js
@@ -3,9 +3,10 @@
 // https://developers.google.com/web/fundamentals/primers/service-workers/
 // chrome: chrome://inspect/#service-workers
 
-var CACHE_NAME = 'dwv-jqmobile-cache-v1';
+var CACHE_NAME = 'dwv-jqmobile-cache-v2';
 var urlsToCache = [
     './',
+    './index.html',
     // css
     './css/style.css',
     // js
@@ -98,7 +99,7 @@ self.addEventListener('install', function (event) {
 // fetch
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then( function (response) {
+        caches.match( event.request, {'ignoreSearch': true} ).then( function (response) {
             // Cache hit - return response
             if (response) {
                 //console.log('Return form cache', event.request.url);
