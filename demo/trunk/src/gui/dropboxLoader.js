e50c5e2007cd024f8d9c09ce7e8d738a827da15c
Travis <travis@travis-ci.org>
2020-12-01 08:36:34 +0000

    Travis build 238 pushed to gh-pages

--- demo/trunk/src/gui/dropboxLoader.js
+++ demo/trunk/src/gui/dropboxLoader.js
@@ -10,16 +10,11 @@ dwvjq.gui = dwvjq.gui || {};
  * @param {Object} app The associated application.
  */
 dwvjq.gui.DropboxLoader = function (app) {
-  // closure to self
-  var self = this;
 
   // drop box class name
   var dropboxClassName = 'dropBox';
   var borderClassName = 'dropBoxBorder';
   var hoverClassName = 'hover';
-
-  // size of the drop box
-  var dropBoxSize = 0;
 
   /**
    * Initialise the drop box.
@@ -28,49 +23,45 @@ dwvjq.gui.DropboxLoader = function (app) {
     // start listening to drag events on the layerContainer
     var layerDiv = app.getElement('layerContainer');
     if (layerDiv) {
+      // show
+      this.showDropbox(true);
+      // start listening to drag events on the layer container
       layerDiv.addEventListener('dragover', onDragOver);
       layerDiv.addEventListener('dragleave', onDragLeave);
       layerDiv.addEventListener('drop', onDrop);
     }
-    // set the initial drop box size
-    var box = app.getElement(dropboxClassName);
-    if (box) {
-      var size = app.getLayerContainerSize();
-      dropBoxSize = (2 * size.height) / 3;
-      box.setAttribute(
-        'style',
-        'width:' + dropBoxSize + 'px;height:' + dropBoxSize + 'px'
-      );
-    }
   };
 
   /**
-   * Hide the drop box gui.
+   * Show or hide the data load drop box.
+   * @param {boolean} show Flag to show or hide.
    */
-  this.hideDropboxElement = function () {
+  this.showDropbox = function (show) {
     var box = app.getElement(dropboxClassName);
     if (box) {
-      // remove size
-      box.removeAttribute('style');
-      // remove border
-      box.className = box.className.replace(' ' + borderClassName, '');
-      box.className = box.className.replace(' ' + hoverClassName, '');
-    }
-  };
-
-  /**
-   * Show the drop box gui.
-   */
-  this.showDropboxElement = function () {
-    var box = app.getElement(dropboxClassName);
-    if (box) {
-      // set size
-      box.setAttribute(
-        'style',
-        'width:' + dropBoxSize + 'px;height:' + dropBoxSize + 'px'
-      );
-      // add border
-      box.className += ' ' + borderClassName;
+      if (show) {
+        // reset css class
+        box.className = dropboxClassName + ' ' + borderClassName;
+        // check content
+        if (box.innerHTML === '') {
+          box.innerHTML = 'Drag and drop data here.';
+        }
+        var size = app.getLayerContainerSize();
+        // set the drop box size
+        var dropBoxSize = 2 * size.height / 3;
+        box.setAttribute(
+          'style',
+          'width:' + dropBoxSize + 'px;height:' + dropBoxSize + 'px');
+      } else {
+        // remove border css class
+        box.className = dropboxClassName;
+        // remove content
+        box.innerHTML = '';
+        // make not visible
+        box.setAttribute(
+          'style',
+          'visible:false;');
+      }
     }
   };
 
@@ -117,7 +108,5 @@ dwvjq.gui.DropboxLoader = function (app) {
     event.preventDefault();
     // load files
     app.loadFiles(event.dataTransfer.files);
-    // hide drop box
-    self.hideDropboxElement();
   }
 }; // dwvjq.gui.dropboxLoader
