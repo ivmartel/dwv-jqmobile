109d2a8ef68228d417e7ad24c6118620f39e914e
ivmartel <ivmartel@gmail.com>
2021-12-13 11:18:50 +0100

    Update to changed dwv 0.30.4 dependency

--- service-worker.js
+++ service-worker.js
@@ -75,7 +75,7 @@ var urlsToCache = [
   // js: dwv
   './node_modules/dwv/dist/dwv.min.js',
   './node_modules/i18next/i18next.min.js',
-  './node_modules/i18next-xhr-backend/i18nextXHRBackend.min.js',
+  './node_modules/i18next-http-backend/i18nextHttpBackend.min.js',
   './node_modules/i18next-browser-languagedetector/' +
     'i18nextBrowserLanguageDetector.min.js',
   './node_modules/jszip/dist/jszip.min.js',
