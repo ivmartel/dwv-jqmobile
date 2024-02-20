d1007f97156a146cbf09b467777fed634e271be3
ivmartel <ivmartel@gmail.com>
2024-02-20 13:09:48 +0100

    Bump version to 0.7.1

--- src/applauncher.js
+++ src/applauncher.js
@@ -11,7 +11,7 @@ function startApp() {
   dwvjq.i18nPage();
 
   // show dwv version
-  dwvjq.gui.appendVersionHtml('0.7.0');
+  dwvjq.gui.appendVersionHtml('0.7.1');
 
   // application options
   var filterList = ['Threshold', 'Sharpen', 'Sobel'];
