52a3af226b187dd7b049029f96295bf616b4a15a
ivmartel <ivmartel@gmail.com>
2025-07-07 16:08:16 +0200

    Release v0.9.3

--- src/applauncher.js
+++ src/applauncher.js
@@ -15,7 +15,7 @@ function startApp() {
   dwvjq.i18n.translatePage();
 
   // show dwv version
-  dwvjq.gui.appendVersionHtml('0.10.0-beta.0');
+  dwvjq.gui.appendVersionHtml('0.9.3');
 
   // application options
   var filterList = ['Threshold', 'Sharpen', 'Sobel'];
